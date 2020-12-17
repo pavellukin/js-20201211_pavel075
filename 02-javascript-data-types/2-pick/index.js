6b420e295ded4e3f877f8f06cbdba916e0459016
Pavel Lukin <paveldon@web.de>
2020-12-17 13:53:29 +0100

    pick выполнил

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,23 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
 
+/*
+const fruits = {
+ apple: 2,
+ orange: 4,
+ banana: 3
+};
+
+console.log(pick(fruits, 'apple', 'banana')); // { apple: 2, banana: 3 }
+*/
+
+export const pick = (obj, ...fields) => {
+    let result = {};
+    for (let [key, value] of Object.entries(obj)) {
+        if(fields.includes(key)) {
+            result[key] = value;
+        }
+    }
+    return result;
 };
